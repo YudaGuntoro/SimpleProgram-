5377a245132b81b5f67cd245dc87e1627e67237c
Yuda Guntoro <47289280+YudaGuntoro@users.noreply.github.com>
2022-06-07 11:49:08 +0700

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -45,4 +45,25 @@ static public void Main(string[] args)
             Console.WriteLine(myNumbers.Max());  // returns the largest value
             Console.WriteLine(myNumbers.Min());  // returns the smallest value
             Console.WriteLine(myNumbers.Sum());  // returns the sum of elements
-}
+            
+        
+            //Array Input
+            int[] arr = new int[10]; 
+            int i;  
+               Console.Write("\n\nRead and Print elements of an array:\n");
+               Console.Write("-----------------------------------------\n");	
+               Console.Write("Input 10 elements in the array :\n");  
+                        
+                for(i=0; i<10; i++)  
+                {  
+                   Console.Write("element - {0} : ",i);
+                   arr[i] = Convert.ToInt32(Console.ReadLine());  		
+                }  
+
+                   Console.Write("\nElements in array are: ");  
+                   for(i=0; i<10; i++)  
+                {  
+                   Console.Write("{0}  ", arr[i]);  
+                } 
+                   Console.Write("\n");	
+
