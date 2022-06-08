72521096819d45bb4ef8a273c824eec59f6a48d5
Yuda Guntoro <47289280+YudaGuntoro@users.noreply.github.com>
2022-06-08 10:07:32 +0700

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -2,22 +2,22 @@
 
 static public void Main(string[] args)
         {
+//===========================================================================================
             var numbers = new[] { 3, 7, 9, 2, 14, 6 }; // 6 Index 
-
             //Length
             Console.WriteLine("Length :" + numbers.Length);
-
+//===========================================================================================
             //Index
             var index = Array.IndexOf(numbers, 6);
             Console.WriteLine("Length :" + index); // 5
-
+//===========================================================================================
             //Clear 
             Array.Clear(numbers, 0, 2);
             foreach (var n in numbers)
             {
                 Console.WriteLine(n);
             }
-
+//===========================================================================================
             //Copy Array
             int[] another = new int[3];
             Array.Copy(numbers, another, 3);
@@ -25,28 +25,27 @@ static public void Main(string[] args)
             {
                 Console.WriteLine(k);
             }
-            
+//===========================================================================================
             //Copy Manage
             Array.Sort(numbers);
             foreach (var l in numbers)
             {
                 Console.WriteLine(l);
             }
-
+//===========================================================================================
             //Reverse
             Array.Reverse(numbers);
             foreach (var m in numbers)
             {
                 Console.WriteLine(m);
             }
-
+//===========================================================================================
             //Value Max.Min.Sum
             int[] myNumbers = { 5, 1, 8, 9 };
             Console.WriteLine(myNumbers.Max());  // returns the largest value
             Console.WriteLine(myNumbers.Min());  // returns the smallest value
             Console.WriteLine(myNumbers.Sum());  // returns the sum of elements
-            
-        
+//===========================================================================================
             //Array Input
             int[] arr = new int[10]; 
             int i;  
@@ -66,7 +65,7 @@ static public void Main(string[] args)
                    Console.Write("{0}  ", arr[i]);  
                 } 
                    Console.Write("\n"); 
-       
+//===========================================================================================
                 // Check array if exist.
                 string[] array = { "cat", "dot", "deves" };
                 // Use Array.Exists in different ways.
@@ -80,27 +79,26 @@ static public void Main(string[] args)
                 Console.WriteLine(b);   //False
                 Console.WriteLine(c);   //True
                 Console.WriteLine(d);   //False
+//===========================================================================================
+                var myInts = new List<int>();
+                    int tryConvert;
+                    while (myInts.Count < 100)
+                    {
+                        Console.WriteLine("Enter a number not already in list: ");
+                        var userEntry = Console.ReadLine();
+                        if (int.TryParse(userEntry, out tryConvert))
+                        {
+                            // if WAS able to convert to a number, now check if in the list
+                            if (myInts.Contains(tryConvert))
+                                Console.WriteLine("Number already in list");
+                            else
+                                myInts.Add(tryConvert);
+                        }
+                        else
+                        {
+                            // allow user to enter a "Q" to quit vs adding all 100 entries
+                            if (userEntry == "Q")
+                                break;
 
-        
-        var myInts = new List<int>();
-            int tryConvert;
-            while (myInts.Count < 100)
-            {
-                Console.WriteLine("Enter a number not already in list: ");
-                var userEntry = Console.ReadLine();
-                if (int.TryParse(userEntry, out tryConvert))
-                {
-                    // if WAS able to convert to a number, now check if in the list
-                    if (myInts.Contains(tryConvert))
-                        Console.WriteLine("Number already in list");
-                    else
-                        myInts.Add(tryConvert);
-                }
-                else
-                {
-                    // allow user to enter a "Q" to quit vs adding all 100 entries
-                    if (userEntry == "Q")
-                        break;
-
-                    Console.WriteLine("Not a valid number");
-                }
+                            Console.WriteLine("Not a valid number");
+                        }
