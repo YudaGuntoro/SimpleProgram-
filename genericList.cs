616fe1c773763aa6d60dc435b3c787b69b2286f0
Yuda Guntoro <47289280+YudaGuntoro@users.noreply.github.com>
2022-06-08 20:08:13 +0700

    Update genericList.cs

--- genericList.cs
+++ genericList.cs
@@ -19,3 +19,49 @@
             Console.Write("Index of 1:" + numbers.IndexOf(1));
             Console.Write("Index of 1:" + numbers.LastIndexOf(1));
         }
+//======================================================================================================
+   var sentence = "bla bli blu ble blo bla bla bla bla bla";
+          const int maxLength = 10;
+
+          if (sentence.Length < maxLength)                //Jika Kalimat kurang dari max then write
+              Console.WriteLine(sentence);                
+
+          var words = sentence.Split(' ');                //Split Space menjadi blabliblu 
+
+          /*string data1 = words[0];
+          string data2 = words[1];
+          string data3 = words[2];
+          */    
+          //Console.Write(data1); Console.Write(data2); Console.WriteLine(data3);
+
+          var totalCharacter = 0;
+          var summaryWords = new List<string>();
+
+          foreach (var word in words)
+          {
+              summaryWords.Add(word);                 // Menambahkan value word kedalam summary words 
+              totalCharacter += word.Length;      
+
+              if (totalCharacter > maxLength)
+              {
+                  break;
+              }
+          }
+          var summary = String.Join(" ", summaryWords) + "...";  // Menggabubgkan hasil Spasi dan summary word +  . . .  (bla bli blu...)
+          Console.WriteLine(summary);
+
+//======================================================================================================
+
+var uniques = GetUniqueNumbers(numbers);
+public static List<int> GetUniqueNumbers(List<int>numbers)
+{
+        var uniques = new List<int>();
+        foreach( var number in numbers)
+        {
+                if (!uniques.Contains(number))
+                        uniques.Add(number);
+        }
+        
+        return uniques;
+}
+        
